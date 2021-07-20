3d5db79ce05e8998134e531643565dae22b684ba
Ramprasad <ramprasadsuthi@gmail.com>
2021-07-20 06:22:45 +0530

    Create arrayDemo.java

---
+++ basicPrograms/arrayDemo.java
@@ -0,0 +1,26 @@
+package basicPrograms;
+
+public class arrayDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String[] cars = {"BMW", "Maruti", "AUDI", "Volvo"};
+		
+		System.out.println("Car name is " + cars[1]);
+		
+		System.out.println("The length of the array is " + cars.length);
+		
+		for(int i=0; i<cars.length; i++ ) {
+			
+			System.out.println("** The Car name is : " + cars[i]);
+		}
+		
+		for (String i : cars) {
+			
+			System.out.println("The car name is : " + i);
+		}
+
+	}
+
+}
