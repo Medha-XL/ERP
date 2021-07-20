662e45d17dc6733b2b24d1a4b65a33d4a446b33a
Ramprasad <ramprasadsuthi@gmail.com>
2021-07-20 06:23:40 +0530

    Create welcomeJava.java

---
+++ basicPrograms/welcomeJava.java
@@ -0,0 +1,11 @@
+package basicPrograms;
+
+public class welcomeJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello, Java !!!");
+	}
+
+}
